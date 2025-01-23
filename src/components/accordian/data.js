bb0c337c708128fb158ae9c15883e373c7bf193f
peterjbone <peterjbone41@gmail.com>
2025-01-23 14:57:49 -0500

    data.js corrected

--- src/components/accordian/data.js
+++ src/components/accordian/data.js
@@ -1,28 +1,25 @@
-const data [
-  { 
-    id: "1",
-    question: "What are accordion components?",
-    answer: "Accordion components are user interface elements used for organizations"
-  },
-  {
+const data = [
+	{
+		id: "1",
+		question: "What are accordion components?",
+		answer: "Accordion components are user interface elements used for organization."
+	},
+	{
+		id: "2",
+		question: "What are they used for?",
+		answer: "They are commonly employed in various contexts, including FAQs."
+	},
+	{
+		id: "3",
+		question: "Accordion as a musical instrument",
+		answer: "The accordion is a musical instrument with a keyboard and bellows."
+	},
+	{
+		id: "4",
+		question: "Can I create an accordion component with a different framework?",
+		answer:
+			"Yes, of course, it is very possible to create an accordion component with another framework."
+	}
+];
 
-    id: '2",
-    question: "What are they used for?",
-    answer:  "They are commonly employed in various contexts, including FAQs"
-  },
-  {
-
-    id: '3",
-    question: "Accordion as a musical instrument",
-    answer:
-    "The accordion is a musical instrument with a keyboard and bellc"
-  },
-{
-  id: 4,
-    question: "Can I create an accordion component with a different fr",
-  answer:
-  "Yes of course, it is very possible to create an accordion component with another framwork"
-},
-]
-
-export default data
+export default data;
