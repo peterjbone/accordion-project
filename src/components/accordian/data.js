163ce38ed06b82cd4b326bca9969844594d08209
peterjbone <peterjbone41@gmail.com>
2025-01-23 14:56:06 -0500

    initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data [
+  { 
+    id: "1",
+    question: "What are accordion components?",
+    answer: "Accordion components are user interface elements used for organizations"
+  },
+  {
+
+    id: '2",
+    question: "What are they used for?",
+    answer:  "They are commonly employed in various contexts, including FAQs"
+  },
+  {
+
+    id: '3",
+    question: "Accordion as a musical instrument",
+    answer:
+    "The accordion is a musical instrument with a keyboard and bellc"
+  },
+{
+  id: 4,
+    question: "Can I create an accordion component with a different fr",
+  answer:
+  "Yes of course, it is very possible to create an accordion component with another framwork"
+},
+]
+
+export default data
